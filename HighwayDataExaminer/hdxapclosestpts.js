c7711394aef4a3136de639666708f9bba202649c
Ma09verr <ma09verr@siena.edu>
2022-06-07 14:03:35 -0400

    Committing before moving to other branch

--- HighwayDataExaminer/hdxapclosestpts.js
+++ HighwayDataExaminer/hdxapclosestpts.js
@@ -10,7 +10,36 @@ var hdxAPClosestPtsAV = {
     // entries for list of AVs
     value: "APClosestPts",
     name: "All Points Closest Pairs",
-    description: "Search for the closest pair of vertices (waypoints)."
+    description: "Search for the closest pair of vertices (waypoints).",
 
-    
+    // ***Most of my code and comments here will be based on my understanding of OOP from CS-225 in Java, 
+    // please feel free to correct any misconceptions or incorrect assumptions I have made!***
+
+    // points is the array of waypoint objects that are read from a TMG file
+    points: null,
+
+    // closest is the array of indexes which correspond to the closest points in the "points" array.
+    closest: [],
+
+    // the distance between the two closest points in the array of points.
+    globalMinD: -1,
+
+
+avActions: [
+    {
+        label: "START",
+        comment: "Initialize all points closest pairs variables",
+        code: function(thisAV) {
+            highlightPseudocode(this.label, visualSettings.visiting);
+            
+        }
+    }
+]
+
+
+}
+
+function ClosestToAll() {
+    this.closest = points.length;
+
 }
