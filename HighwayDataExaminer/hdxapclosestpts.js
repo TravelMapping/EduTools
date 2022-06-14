0659affaf6c115a7b6077c9448af7d1a33754f22
Ma09verr <ma09verr@siena.edu>
2022-06-14 17:04:34 -0400

    Started pseudocode for all pts closest pairs AV

--- HighwayDataExaminer/hdxapclosestpts.js
+++ HighwayDataExaminer/hdxapclosestpts.js
@@ -35,12 +35,41 @@ var hdxAPClosestPtsAV = {
             comment: "Initialize all points closest pairs variables",
             code: function (thisAV) {
                 highlightPseudocode(this.label, visualSettings.visiting);
-                
+
             }
         }
 
 
-    ]
+    ],
+
+    prepToStart() {
+        hdxAV.algStat.innerHTML = "Initializing";
+        
+        //we want only vertices for this algorithm
+        initWaypointsAndConnections(true, false, visualSettings.undiscovered);
+
+        
+
+        this.code = '<table class="pseudocode"><tr id="START" class="pseudocode"><td class="pseudocode">';
+
+        //pseudocode for the start state
+        this.code += `closestVertex &larr; []`;
+    
+
+        //pseudocode for the top of the for loop
+        this.code += '</td></tr>' +
+            pcEntry(0, 'for (v<sub>1</sub> &larr; 0 to |V| - 1)',"v1ForLoopTop");
+        this.code += '</td></tr>' +
+            pcEntry(1, 'v<sub>closest</sub> &larr; -1<br />', "InitVars") +
+            pcEntry(1, 'd<sub>closest</sub> &larr; &infin;<br />', "InitVars") +
+            pcEntry(1, 'for (v2 &larr; 0 to |V| - 1)', "v2ForLoopTop");
+        this.code += '</td></tr>' +
+            pcEntry(2,'if (v<sub>1</sub> &ne; v<sub>2</sub>)') +
+            pcEntry(3, 'd &larr; dist(v<sub>1</sub>, v<sub>2</sub>)') +
+            pcEntry(3, 'if(d < d<sub>closest</sub>)');
+            
+
+}
 
 
 }
