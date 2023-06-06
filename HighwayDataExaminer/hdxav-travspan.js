ab93f3d63bbc9e6266ae61cc9347c945c251fa35
MikePlekan <me26plek@siena.edu>
2023-06-06 12:19:56 -0400

    Functional A* AV

--- HighwayDataExaminer/hdxav-travspan.js
+++ HighwayDataExaminer/hdxav-travspan.js
@@ -1418,9 +1418,14 @@ hdxAstarAV.comparator = function(a, b) {
 // to get from the vertex being visited
 hdxAstarAV.valForLDVEntry = function(oldEntry, nextNeighbor) {
     let pathLength = oldEntry.val - convertToCurrentUnits(distanceInMiles(waypoints[oldEntry.vIndex].lat, waypoints[oldEntry.vIndex].lon, waypoints[this.endingVertex].lat, waypoints[this.endingVertex].lon));
-    return pathLength 
-    + convertToCurrentUnits(edgeLengthInMiles(graphEdges[nextNeighbor.via])) 
-    + convertToCurrentUnits(distanceInMiles(waypoints[nextNeighbor.to].lat, waypoints[nextNeighbor.to].lon, waypoints[this.endingVertex].lat, waypoints[this.endingVertex].lon));
+    pathLength = oldEntry.val!=0?pathLength:0;//conditional is for starting node
+    let newpath = convertToCurrentUnits(edgeLengthInMiles(graphEdges[nextNeighbor.via])); 
+    let distance = convertToCurrentUnits(distanceInMiles(waypoints[nextNeighbor.to].lat, waypoints[nextNeighbor.to].lon, waypoints[this.endingVertex].lat, waypoints[this.endingVertex].lon));
+    console.log("oldEntry.val: "+oldEntry.val)
+    console.log("pathLength: "+pathLength)
+    console.log("newpath:"+newpath)
+    console.log("distance:"+distance)
+    return pathLength+newpath+distance;
 };
 
 // helper function to help build pseudocode
